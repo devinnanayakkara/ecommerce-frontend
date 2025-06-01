eb9cf2aece954a0be7f9bba458169ff862c1fa7b
Devin Nanayakkara <devin.20232550@iit.ac.lk>
2025-06-01 10:56:20 +0530

    Fix API baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ecommerce-backend-xz8d.onrender.com',
+    baseURL: 'https://ecommerce-backend-xz8d.onrender.com/api',
 });
 
 export default api; 
